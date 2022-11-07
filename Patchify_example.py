e592494ecf757985585c95477c6f325528e7fb6e
Wright-oh <109503209+Wright-oh@users.noreply.github.com>
2022-11-08 09:16:19 +1000

    Update Patchify_example.py

--- Patchify_example.py
+++ Patchify_example.py
@@ -1,8 +1,20 @@
 from patchify import patchify
-import tiffile as tiff
-from skimage import img_as_float
+from skimage import img_as_ubyte
+import os
+import tifffile as tiff
+import cv2
+# Adjust this code to patch individual images.
 
-image = img_as_float(tiff.imread("**File Path**"))
-print(image.shape)
-patches = patchify(image, (5,5), step=5)
-print(patches.shape)
+# Open image and cut it into smaller images for processing
+Raw_image = 'LOECs/IMAGE_1.tif'
+img_large = img_as_ubyte(cv2.imread('LOECs/IMAGE_1.tif', 1))
+filename, file_extension = os.path.splitext(Raw_image)
+print('filename is ' + filename)
+print('file ext is ' + file_extension)
+
+img_patch = patchify(img_large, (256, 256, 3), step=256)  # Step=256 for 256 patches means no overlap
+
+for i in range(img_patch.shape[0]):
+    for j in range(img_patch.shape[1]):
+        single_patch_img = img_patch[i, j, :, :]
+        tiff.imwrite('LOECs/Output_image/' + 'Output_image_1_' + str(i) + '_' + str(j)+".tif", single_patch_img)
